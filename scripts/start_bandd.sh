85c62bced12ebcb2ba1805dd1c937c5069d36c84
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2021-02-01 14:49:28 +0700

    fix start bandd script

--- scripts/start_bandd.sh
+++ scripts/start_bandd.sh
@@ -1,5 +1,5 @@
-cp ./docker-config/single-validator/priv_validator_key.json ~/.bandd/config/priv_validator_key.json
-cp ./docker-config/single-validator/node_key.json ~/.bandd/config/node_key.json
+cp ./docker-config/single-validator/priv_validator_key.json ~/.band/config/priv_validator_key.json
+cp ./docker-config/single-validator/node_key.json ~/.band/config/node_key.json
 
 dropdb my_db
 createdb my_db
