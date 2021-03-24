3ef0b25593d671bd00d7bf36c78f164e1aa1558d
Songwong Tasneeyapant <nat@Nats-MacBook-Pro.local>
2021-03-23 22:20:25 -0500

    add sealandinitialize

--- app/app.go
+++ app/app.go
@@ -422,6 +422,15 @@ func NewBandApp(
 		if err != nil {
 			tmos.Exit(err.Error())
 		}
+		// Initialize and seal the capability keeper so all persistent capabilities
+		// are loaded in-memory and prevent any further modules from creating scoped
+		// sub-keepers.
+		// This must be done during creation of baseapp rather than in InitChain so
+		// that in-memory capabilities get regenerated on app restart.
+		// Note that since this reads from the store, we can only perform it when
+		// `loadLatest` is set to true.
+		ctx := app.BaseApp.NewUncachedContext(true, tmproto.Header{})
+		app.CapabilityKeeper.InitializeAndSeal(ctx)
 	}
 
 	app.ScopedIBCKeeper = scopedIBCKeeper
