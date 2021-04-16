64d3f96bb19c137e8082782c2ede8f2dbe99cfa8
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2021-04-16 14:26:45 +0700

    add initial height

--- cmd/bandd/cmd/migrate.go
+++ cmd/bandd/cmd/migrate.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"time"
 
 	"github.com/cosmos/cosmos-sdk/client"
@@ -15,12 +16,38 @@ import (
 	"github.com/spf13/cobra"
 	tmjson "github.com/tendermint/tendermint/libs/json"
 	tmtypes "github.com/tendermint/tendermint/types"
+
+	bandapp "github.com/bandprotocol/chain/app"
+	oracletypes "github.com/bandprotocol/chain/x/oracle/types"
 )
 
 const (
-	flagGenesisTime = "genesis-time"
-	flagChainID     = "chain-id"
+	flagGenesisTime   = "genesis-time"
+	flagChainID       = "chain-id"
+	flagInitialHeight = "initial-height"
 )
+
+// GenesisDocFromFile reads JSON data from a file and unmarshalls it into a GenesisDoc.
+func GenesisDocFromFile(genDocFile string) (*tmtypes.GenesisDoc, error) {
+	jsonBlob, err := ioutil.ReadFile(genDocFile)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't read GenesisDoc file: %w", err)
+	}
+	genDoc, err := tmtypes.GenesisDocFromJSON(jsonBlob)
+	if err != nil {
+		return nil, fmt.Errorf("error reading GenesisDoc at %s: %w", genDocFile, err)
+	}
+
+	genDoc.ConsensusParams.Block.MaxBytes = 1000000 // 1M bytes
+	genDoc.ConsensusParams.Block.MaxGas = 40000000  // 40M gas
+	genDoc.ConsensusParams.Block.TimeIotaMs = 1000  // 1 second
+
+	if err := genDoc.ValidateAndComplete(); err != nil {
+		return nil, err
+	}
+
+	return genDoc, nil
+}
 
 // MigrateGenesisCmd returns a command to execute genesis state migration.
 // nolint: funlen
@@ -31,7 +58,7 @@ func MigrateGenesisCmd() *cobra.Command {
 		Long: fmt.Sprintf(`Migrate the the Guanyu version into Laozi version and print to STDOUT.
 
 Example:
-$ %s migrate /path/to/genesis.json --chain-id=band-guanyu --genesis-time=2020-08-11T17:00:00Z
+$ %s migrate /path/to/genesis.json --chain-id=band-laozi --genesis-time=2020-08-11T17:00:00Z
 `, version.AppName),
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -40,20 +67,20 @@ $ %s migrate /path/to/genesis.json --chain-id=band-guanyu --genesis-time=2020-08
 			var err error
 
 			importGenesis := args[0]
-			genDoc, err := tmtypes.GenesisDocFromFile(importGenesis)
+			genDoc, err := GenesisDocFromFile(importGenesis)
 			if err != nil {
 				return errors.Wrapf(err, "failed to read genesis document from file %s", importGenesis)
 			}
-
-			genDoc.ConsensusParams.Evidence = tmtypes.DefaultEvidenceParams()
 
 			var initialState genutiltypes.AppMap
 			if err := json.Unmarshal(genDoc.AppState, &initialState); err != nil {
 				return errors.Wrap(err, "failed to JSON unmarshal initial genesis state")
 			}
 
-			// Migrate from Wenchang (0.39 like genesis file) to cosmos-sdk v0.40
+			// Migrate from guanyu (0.39 like genesis file) to cosmos-sdk v0.40
 			newGenState := v040.Migrate(initialState, clientCtx)
+			defaultLaozi := bandapp.NewDefaultGenesisState()
+			initialState[oracletypes.ModuleName] = defaultLaozi[oracletypes.ModuleName]
 
 			genDoc.AppState, err = json.Marshal(newGenState)
 			if err != nil {
@@ -77,6 +104,9 @@ $ %s migrate /path/to/genesis.json --chain-id=band-guanyu --genesis-time=2020-08
 				genDoc.ChainID = chainID
 			}
 
+			initialHeight, _ := cmd.Flags().GetInt(flagInitialHeight)
+			genDoc.InitialHeight = int64(initialHeight)
+
 			bz, err := tmjson.Marshal(genDoc)
 			if err != nil {
 				return errors.Wrap(err, "failed to marshal genesis doc")
@@ -93,5 +123,7 @@ $ %s migrate /path/to/genesis.json --chain-id=band-guanyu --genesis-time=2020-08
 	}
 	cmd.Flags().String(flagGenesisTime, "", "override genesis_time with this flag")
 	cmd.Flags().String(flagChainID, "band-laozi", "override chain_id with this flag")
+	cmd.Flags().Int(flagInitialHeight, 0, "Set the starting height for the chain")
+
 	return cmd
 }
