d4c2959b33b485e0623d8ff333a49383d7e0b0f2
Songwong Tasneeyapant <songwong@bandprotocol.com>
2021-04-21 22:41:43 -0500

    update rpc path

--- x/oracle/types/query.pb.gw.go
+++ x/oracle/types/query.pb.gw.go
@@ -1132,7 +1132,7 @@ var (
 
 	pattern_Query_RequestPrice_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"oracle", "request_prices"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Query_RequestPool_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 1, 0, 4, 1, 5, 3, 1, 0, 4, 1, 5, 4}, []string{"oracle", "request_pool", "request_key", "port_id", "channel_id"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_RequestPool_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 1, 0, 4, 1, 5, 4, 1, 0, 4, 1, 5, 5}, []string{"oracle", "v1", "request_pool", "request_key", "port_id", "channel_id"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
