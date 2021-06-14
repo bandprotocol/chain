90231bdfe0cb74d150f160991c52681cc4fe5bc2
taobun <bun@bandprotocol.com>
2021-06-14 16:00:11 +0700

    Remove unused function in types/genesis.go

--- x/oracle/types/genesis.go
+++ x/oracle/types/genesis.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 
 	"github.com/cosmos/cosmos-sdk/codec"
-	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 )
 
 // NewGenesisState creates a new GenesisState instanc e
@@ -36,16 +35,6 @@ func GetGenesisStateFromAppState(cdc codec.JSONMarshaler, appState map[string]js
 	}
 
 	return &genesisState
-}
-
-// UnpackInterfaces implements UnpackInterfacesMessage.UnpackInterfaces
-func (g GenesisState) UnpackInterfaces(c codectypes.AnyUnpacker) error {
-	// for i := range g.Validators {
-	// 	if err := g.Validators[i].UnpackInterfaces(c); err != nil {
-	// 		return err
-	// 	}
-	// }
-	return nil
 }
 
 // Validate performs basic genesis state validation returning an error upon any
