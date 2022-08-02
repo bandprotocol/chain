d82df24df27ce6624d53232ec619514e7a354c1a
satawatnack <s.nack1919@gmail.com>
2022-08-02 15:41:40 +0700

    add wg

--- x/oracle/abci.go
+++ x/oracle/abci.go
@@ -1,6 +1,8 @@
 package oracle
 
 import (
+	"sync"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 
@@ -19,15 +21,19 @@ func handleBeginBlock(ctx sdk.Context, req abci.RequestBeginBlock, k keeper.Keep
 
 // handleEndBlock cleans up the state during end block. See comment in the implementation!
 func handleEndBlock(ctx sdk.Context, k keeper.Keeper) {
-	const MAX_CONCURRENT_JOBS = 4
+	const MAX_CONCURRENT_JOBS = 5
 	jobc := make(chan struct{}, MAX_CONCURRENT_JOBS)
+
+	var wg sync.WaitGroup
 
 	// Loops through all requests in the resolvable list to parallel resolve all of them!
 	for _, reqID := range k.GetPendingResolveList(ctx) {
+		wg.Add(1)
 		// Create an empty struct to signal when the job finishes
 		jobc <- struct{}{}
 		go k.ResolveRequest(ctx, reqID, jobc)
 	}
+	wg.Wait()
 
 	// Once all the requests are resolved, we can clear the list.
 	k.SetPendingResolveList(ctx, []types.RequestID{})
