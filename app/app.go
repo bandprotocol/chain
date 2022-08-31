966c34ffac2b0ed089dcdbbbc8ca799a95214b4a
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2022-08-31 12:24:03 +0700

    remove duplicate comment

--- app/app.go
+++ app/app.go
@@ -434,14 +434,12 @@ func NewBandApp(
 
 	app.IBCKeeper.SetRouter(ibcRouter)
 
-	// create evidence keeper with router.
 	// create evidence keeper with router
 	evidenceKeeper := evidencekeeper.NewKeeper(
 		appCodec, keys[evidencetypes.StoreKey], &app.StakingKeeper, app.SlashingKeeper,
 	)
 	// If evidence needs to be handled for the app, set routes in router here and seal
 	app.EvidenceKeeper = *evidenceKeeper
-	/****  Module Options ****/
 
 	/****  Module Options ****/
 	// NOTE: we may consider parsing `appOpts` inside module constructors. For the moment
