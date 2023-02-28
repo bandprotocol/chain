40bbfbfd094e7b33e7ac268e9f64eb9d10644d74
satawatnack <s.nack1919@gmail.com>
2023-02-28 14:21:14 +0700

    test consensus

--- app/constants.go
+++ app/constants.go
@@ -3,7 +3,8 @@ package band
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
 // Its intended to be .0025 uband / gas
+// TODO: change it back from 0.05 to 0.0025
 var (
 	Denom                   = "uband"
-	ConsensusMinFee sdk.Dec = sdk.NewDecWithPrec(25, 4)
+	ConsensusMinFee sdk.Dec = sdk.NewDecWithPrec(5, 2)
 )
