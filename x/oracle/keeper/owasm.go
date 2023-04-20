3ab161b6545673c6824815923e7a8a23d4c6a309
satawatnack <s.nack1919@gmail.com>
2023-04-20 13:20:17 +0700

    use math ceil

--- x/oracle/keeper/owasm.go
+++ x/oracle/keeper/owasm.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"encoding/hex"
 	"fmt"
+	"math"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -21,7 +22,7 @@ func ConvertToOwasmGas(cosmos uint64) uint64 {
 }
 
 func ConvertToGas(owasm uint64) uint64 {
-	return uint64(owasm / gasConversionFactor)
+	return uint64(math.Ceil(float64(owasm) / float64(gasConversionFactor)))
 }
 
 // GetSpanSize return maximum value between MaxReportDataSize and MaxCallDataSize
