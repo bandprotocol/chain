9882fe1fbb0a33967331bf91e2c87deb0e349bec
satawatnack <s.nack1919@gmail.com>
2023-04-24 17:55:22 +0700

    add members & fix round

--- x/tss/types/events.go
+++ x/tss/types/events.go
@@ -4,6 +4,7 @@ const (
 	EventTypeCreateGroup = "create_group"
 
 	AttributeKeyGroupID   = "group_id"
+	AttributeMembers      = "members"
 	AttributeKeySize      = "size"
 	AttributeKeyThreshold = "threshold"
 	AttributeKeyPubKey    = "pub_key"
