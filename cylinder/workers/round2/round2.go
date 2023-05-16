dc229c6d02282bed7c0ccdc39d873750d8bbc54b
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2023-05-17 02:24:25 +0700

    add comment

--- cylinder/workers/round2/round2.go
+++ cylinder/workers/round2/round2.go
@@ -14,19 +14,19 @@ import (
 	tmtypes "github.com/tendermint/tendermint/types"
 )
 
+// Round2 is a worker responsible for round2 in the DKG process of TSS module
 type Round2 struct {
 	context *cylinder.Context
-
-	logger *logger.Logger
-	client *client.Client
-
+	logger  *logger.Logger
+	client  *client.Client
 	eventCh <-chan ctypes.ResultEvent
 }
 
 var _ cylinder.Worker = &Round2{}
 
+// New creates a new instance of the Round2 worker.
+// It initializes the necessary components and returns the created Round2 instance or an error if initialization fails.
 func New(ctx *cylinder.Context) (*Round2, error) {
-	// create http client
 	cli, err := client.New(ctx.Config, ctx.Keyring)
 	if err != nil {
 		return nil, err
@@ -39,6 +39,8 @@ func New(ctx *cylinder.Context) (*Round2, error) {
 	}, nil
 }
 
+// subscribe subscribes to the round2 events and initializes the event channel for receiving events.
+// It returns an error if the subscription fails.
 func (r *Round2) subscribe() error {
 	var err error
 	r.eventCh, err = r.client.Subscribe(
@@ -53,6 +55,8 @@ func (r *Round2) subscribe() error {
 	return err
 }
 
+// handleTxResult handles the result of a transaction.
+// It extracts the relevant message logs from the transaction result and processes the events.
 func (r *Round2) handleTxResult(txResult abci.TxResult) {
 	msgLogs, err := event.GetMessageLogs(txResult)
 	if err != nil {
@@ -71,11 +75,12 @@ func (r *Round2) handleTxResult(txResult abci.TxResult) {
 	}
 }
 
+// handleEvent processes an incoming group event.
 func (r *Round2) handleEvent(event *Event) {
 	logger := r.logger.With("gid", event.GroupID)
 	logger.Info(":delivery_truck: Processing incoming group event")
 
-	// set group data
+	// Set group data
 	group, err := r.context.Store.GetGroup(event.GroupID)
 	if err != nil {
 		logger.Error(":cold_sweat: Failed to find group in store: %s", err.Error())
@@ -88,13 +93,13 @@ func (r *Round2) handleEvent(event *Event) {
 		return
 	}
 
-	// get all one time public keys in the group
+	// Get all one time public keys in the group
 	oneTimePubKeys := make(tss.PublicKeys, gr.Group.Size_)
 	for mid, commitment := range gr.AllRound1Commitments {
 		oneTimePubKeys[mid-1] = commitment.OneTimePubKey
 	}
 
-	// calculate encrypted secret shares
+	// Compute encrypted secret shares
 	encSecretShares, err := tss.ComputeEncryptedSecretShares(
 		group.MemberID,
 		group.OneTimePrivKey,
@@ -106,7 +111,7 @@ func (r *Round2) handleEvent(event *Event) {
 		return
 	}
 
-	// generate message
+	// Generate message
 	// TODO-CYLINDER: generate round2 message
 	fmt.Printf("%+v", encSecretShares)
 	msg := &types.MsgSubmitDKGRound1{}
@@ -114,6 +119,8 @@ func (r *Round2) handleEvent(event *Event) {
 	r.context.MsgCh <- msg
 }
 
+// Start starts the Round2 worker.
+// It subscribes to round2 events and starts processing incoming events.
 func (r *Round2) Start() {
 	r.logger.Info("start")
 
@@ -123,12 +130,12 @@ func (r *Round2) Start() {
 		return
 	}
 
-	for {
-		ev := <-r.eventCh
+	for ev := range r.eventCh {
 		go r.handleTxResult(ev.Data.(tmtypes.EventDataTx).TxResult)
 	}
 }
 
+// Stop stops the Round2 worker.
 func (r *Round2) Stop() {
 	r.logger.Info("stop")
 	r.client.Stop()
