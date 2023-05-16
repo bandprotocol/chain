6acec187e1e2190cfe67bedf32049a6ccef1100b
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2023-05-17 00:58:00 +0700

    write test and comment for round2,3 tss lib

--- pkg/tss/round2_test.go
+++ pkg/tss/round2_test.go
@@ -1,25 +1,39 @@
 package tss_test
 
 import (
-	"testing"
+	"encoding/hex"
 
 	"github.com/bandprotocol/chain/v2/pkg/tss"
-	"github.com/stretchr/testify/assert"
 )
 
-func TestCalculateEncryptedSecretShares(t *testing.T) {
-	// n = 4
+func (suite *TSSTestSuite) TestComputeEncryptedSecretShares() {
+	// n = 2
 	// t = 2
-	// 0-3 for people in groups, 4-5 for coefficients
-	kps, err := tss.GenerateKeyPairs(6)
-	assert.NoError(t, err)
-
 	scalars, err := tss.ComputeEncryptedSecretShares(
 		1,
-		kps[0].PrivateKey,
-		tss.PublicKeys{kps[0].PublicKey, kps[1].PublicKey, kps[2].PublicKey, kps[3].PublicKey},
-		tss.Scalars{tss.Scalar(kps[4].PrivateKey), tss.Scalar(kps[5].PrivateKey)},
+		suite.kpI.PrivateKey,
+		tss.PublicKeys{suite.kpI.PublicKey, suite.kpJ.PublicKey},
+		suite.scalars,
 	)
-	assert.NoError(t, err)
-	assert.Equal(t, 3, len(scalars))
+	suite.Require().NoError(err)
+	suite.Require().Equal(1, len(scalars))
+}
+
+func (suite *TSSTestSuite) TestEncryptSecretShares() {
+	secret, err := hex.DecodeString("765613e9f908c8379b2b9e29ac0fda7b8bcc5e3fe34e010dc7a1491bfc0f96a1")
+	suite.Require().NoError(err)
+
+	keySym, err := hex.DecodeString("03bc213e4251592d29c070e4c31b980d150e755ec848afa4c06730ec1dcd09c482")
+	suite.Require().NoError(err)
+
+	encSecretShares, err := tss.EncryptSecretShares(tss.Scalars{secret}, tss.PublicKeys{keySym})
+	suite.Require().NoError(err)
+	suite.Require().
+		Equal("0fd56e0e97d585edf49e82e3a7abe4ac9fc2bdd1b9681c840341849eb0eb852d", hex.EncodeToString(encSecretShares[0]))
+}
+
+func (suite *TSSTestSuite) TestComputeSecretShare() {
+	secret := tss.ComputeSecretShare(suite.scalars, 2)
+	suite.Require().
+		Equal("765613e9f908c8379b2b9e29ac0fda7b8bcc5e3fe34e010dc7a1491bfc0f96a1", hex.EncodeToString(secret))
 }
