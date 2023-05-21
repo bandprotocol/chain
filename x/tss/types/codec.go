a0c2eaf354babc48d85b339487dae132ae9c8072
satawatnack <s.nack1919@gmail.com>
2023-05-21 22:58:00 +0700

    init round 3

--- x/tss/types/codec.go
+++ x/tss/types/codec.go
@@ -10,6 +10,8 @@ func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(&MsgCreateGroup{}, "tss/CreateGroup", nil)
 	cdc.RegisterConcrete(&MsgSubmitDKGRound1{}, "tss/SubmitDKGRound1", nil)
 	cdc.RegisterConcrete(&MsgSubmitDKGRound2{}, "tss/SubmitDKGRound2", nil)
+	cdc.RegisterConcrete(&MsgComplain{}, "tss/Complain", nil)
+	cdc.RegisterConcrete(&MsgConfirm{}, "tss/Confirm", nil)
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
@@ -17,6 +19,8 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 		&MsgCreateGroup{},
 		&MsgSubmitDKGRound1{},
 		&MsgSubmitDKGRound2{},
+		&MsgComplain{},
+		&MsgConfirm{},
 	)
 }
 
