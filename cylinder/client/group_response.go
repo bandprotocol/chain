b2b83e633129dcf2ebc8f5006c8d0aebc200ab31
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2023-05-23 20:49:03 +0700

    update

--- cylinder/client/group_response.go
+++ cylinder/client/group_response.go
@@ -9,20 +9,24 @@ import (
 
 // GroupResponse wraps the types.QueryGroupResponse to provide additional helper methods.
 type GroupResponse struct {
-	*types.QueryGroupResponse
+	types.QueryGroupResponse
 }
 
 // NewGroupResponse creates a new instance of GroupResponse.
 func NewGroupResponse(gr *types.QueryGroupResponse) *GroupResponse {
-	return &GroupResponse{gr}
+	return &GroupResponse{*gr}
 }
 
 // GetRound1Data retrieves the Round1Commitment for the specified member ID.
-func (gr *GroupResponse) GetRound1Data(mid tss.MemberID) (*types.Round1Data, error) {
-	data := gr.AllRound1Data[uint64(mid)-1]
-	if data == nil {
-		return nil, fmt.Errorf("No Round1Commitment from MemberID(%d)", mid)
+func (gr *GroupResponse) GetRound1Data(mid tss.MemberID) (types.Round1Data, error) {
+	if int(mid) > len(gr.AllRound1Data) {
+		return types.Round1Data{}, fmt.Errorf("No MemberID(%d) in the group", mid)
 	}
 
-	return data, nil
+	data := gr.AllRound1Data[uint64(mid)-1]
+	if data == nil {
+		return types.Round1Data{}, fmt.Errorf("No Round1Data from MemberID(%d)", mid)
+	}
+
+	return *data, nil
 }
