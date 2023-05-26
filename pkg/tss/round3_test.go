dbca2e037a7d5ba905d5b6e7ac51f5ebd3c193a3
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2023-05-26 19:13:51 +0700

    final signing

--- pkg/tss/round3_test.go
+++ pkg/tss/round3_test.go
@@ -8,39 +8,47 @@ import (
 )
 
 func (suite *TSSTestSuite) TestComputeOwnPublicKey() {
-	pubKey, err := tss.ComputeOwnPublicKey(suite.points, suite.mid)
+	point1, err := tss.SumPoints(suite.member1.CoefficientsCommit[0], suite.member2.CoefficientsCommit[0])
 	suite.Require().NoError(err)
-	suite.Require().
-		Equal("023704dcdb774ed4fd0841ded5757211fe5a6f7637c4f9a1346b5b20e2524d12e5", hex.EncodeToString(pubKey))
+	point2, err := tss.SumPoints(suite.member1.CoefficientsCommit[1], suite.member2.CoefficientsCommit[1])
+	suite.Require().NoError(err)
+
+	pubKey, err := tss.ComputeOwnPublicKey(
+		tss.Points{point1, point2},
+		suite.member1.mid,
+	)
+	suite.Require().NoError(err)
+	suite.Require().Equal(suite.member1.ownKey.PublicKey, pubKey)
 }
 
 func (suite *TSSTestSuite) TestComputeGroupPublicKey() {
-	pubKey, err := tss.ComputeGroupPublicKey(suite.points)
+	pubKey, err := tss.ComputeGroupPublicKey(
+		tss.Points{suite.member1.CoefficientsCommit[0], suite.member2.CoefficientsCommit[0]},
+	)
 	suite.Require().NoError(err)
-	suite.Require().
-		Equal("023704dcdb774ed4fd0841ded5757211fe5a6f7637c4f9a1346b5b20e2524d12e5", hex.EncodeToString(pubKey))
+	suite.Require().Equal(suite.groupPubKey, pubKey)
 }
 
 func (suite *TSSTestSuite) TestComputeOwnPrivateKey() {
-	privKey, err := tss.ComputeOwnPrivateKey(suite.scalars)
+	ownSecret, err := tss.ComputeSecretShare(suite.member1.Coefficients, 1)
+	privKey, err := tss.ComputeOwnPrivateKey(tss.Scalars{ownSecret, suite.member2.secretShares[0]})
 	suite.Require().NoError(err)
-	suite.Require().
-		Equal("a537d3a6166c8efdf89d76bda2392e228f567ef12fe0632c0445e66cfdf53f02", hex.EncodeToString(privKey))
+	suite.Require().Equal(suite.member1.ownKey.PrivateKey, privKey)
 }
 
 func (suite *TSSTestSuite) TestVerifySecretShare() {
-	secret, err := tss.ComputeSecretShare(suite.scalars, suite.mid)
+	secret, err := tss.ComputeSecretShare(suite.member1.Coefficients, suite.member1.mid)
 	suite.Require().NoError(err)
 
-	err = tss.VerifySecretShare(suite.mid, secret, suite.points)
+	err = tss.VerifySecretShare(suite.member1.mid, secret, suite.member1.CoefficientsCommit)
 	suite.Require().NoError(err)
 }
 
 func (suite *TSSTestSuite) TestComputeSecretShareCommit() {
-	secretCommit, err := tss.ComputeSecretShareCommit(suite.points, suite.mid)
+	secretCommit, err := tss.ComputeSecretShareCommit(suite.member1.CoefficientsCommit, suite.member1.mid)
 	suite.Require().NoError(err)
 	suite.Require().
-		Equal("023704dcdb774ed4fd0841ded5757211fe5a6f7637c4f9a1346b5b20e2524d12e5", hex.EncodeToString(secretCommit))
+		Equal("033c55180665bd1ec4467a3872da2f574f9eafe2aa28a605315b49860a5215f849", hex.EncodeToString(secretCommit))
 }
 
 func (suite *TSSTestSuite) TestDecryptSecretShares() {
@@ -49,69 +57,94 @@ func (suite *TSSTestSuite) TestDecryptSecretShares() {
 		tss.ParseScalar(new(secp256k1.ModNScalar).SetInt(2)),
 	}
 
+	pubKeys := tss.PublicKeys{suite.member1.OneTimePubKey, suite.member2.OneTimePubKey}
+
 	encSecrets, err := tss.EncryptSecretShares(
 		expectedSecrets,
-		tss.PublicKeys{suite.kpI.PublicKey, suite.kpJ.PublicKey},
+		pubKeys,
 	)
 	suite.Require().NoError(err)
 
-	secrets, err := tss.DecryptSecretShares(encSecrets, tss.PublicKeys{suite.kpI.PublicKey, suite.kpJ.PublicKey})
+	secrets, err := tss.DecryptSecretShares(encSecrets, pubKeys)
 	suite.Require().NoError(err)
-
 	suite.Require().Equal(expectedSecrets, secrets)
 }
 
-func (suite *TSSTestSuite) TestVerifyOwnPubKeySig() {
+func (suite *TSSTestSuite) TestSignAndVerifyOwnPubKey() {
 	// Sign
-	sig, err := tss.SignOwnPublickey(suite.mid, suite.dkgContext, suite.kpI.PublicKey, suite.kpI.PrivateKey)
+	sig, err := tss.SignOwnPublickey(
+		suite.member1.mid,
+		suite.groupDKGContext,
+		suite.member1.ownKey.PublicKey,
+		suite.member1.ownKey.PrivateKey,
+	)
 	suite.Require().NoError(err)
 
 	// Success case
-	err = tss.VerifyOwnPubKeySig(suite.mid, suite.dkgContext, sig, suite.kpI.PublicKey)
+	err = tss.VerifyOwnPubKeySig(suite.member1.mid, suite.groupDKGContext, sig, suite.member1.ownKey.PublicKey)
 	suite.Require().NoError(err)
-}
-
-func (suite *TSSTestSuite) TestVerifyComplainSig() {
-	// Sign
-	sig, keySym, nonceSym, err := tss.SignComplain(suite.kpI.PublicKey, suite.kpJ.PublicKey, suite.kpI.PrivateKey)
-	suite.Require().NoError(err)
-
-	// Success case
-	err = tss.VerifyComplainSig(suite.kpI.PublicKey, suite.kpJ.PublicKey, keySym, nonceSym, sig)
-	suite.Require().NoError(err)
-
-	// Wrong public key I case
-	err = tss.VerifyComplainSig(suite.fakeKp.PublicKey, suite.kpJ.PublicKey, keySym, nonceSym, sig)
-	suite.Require().Error(err)
-
-	// Wrong public key J case
-	err = tss.VerifyComplainSig(suite.kpI.PublicKey, suite.fakeKp.PublicKey, keySym, nonceSym, sig)
-	suite.Require().Error(err)
-
-	// Wrong key sym case
-	err = tss.VerifyComplainSig(suite.kpI.PublicKey, suite.kpJ.PublicKey, suite.fakeKp.PublicKey, nonceSym, sig)
-	suite.Require().Error(err)
-
-	// Wrong nonce sym case
-	err = tss.VerifyComplainSig(suite.kpI.PublicKey, suite.kpJ.PublicKey, keySym, suite.fakeKp.PublicKey, sig)
-	suite.Require().Error(err)
 }
 
 func (suite *TSSTestSuite) TestGenerateMessageOwnPublicKey() {
-	challenge := tss.GenerateMessageOwnPublicKey(suite.mid, suite.dkgContext, suite.kpI.PublicKey)
+	challenge := tss.GenerateMessageOwnPublicKey(
+		suite.member1.mid,
+		suite.groupDKGContext,
+		suite.member1.ownKey.PublicKey,
+	)
 	suite.Require().Equal(
-		"726f756e64334f776e5075624b65790000000000000001a1cdd234702bbdbd8a4fa9fc17f2a83d569f553ae4bd1755985e5039532d108c03936f4b0644c78245124c19c9378e307cd955b227ee59c9ba16f4c7426c6418aa",
+		"726f756e64334f776e5075624b65790000000000000001a1cdd234702bbdbd8a4fa9fc17f2a83d569f553ae4bd1755985e5039532d108c0268c34a74f75ea26f3eba73a44afdaaa5e4704baa6f58d6e1ab831a5608e4dae4",
 		hex.EncodeToString(challenge),
 	)
 }
-
-func (suite *TSSTestSuite) TestGenerateMessageComplain() {
-	keySym, err := tss.ComputeKeySym(suite.kpI.PrivateKey, suite.kpJ.PublicKey)
+func (suite *TSSTestSuite) TestSignAndVerifyComplain() {
+	// Sign
+	sig, keySym, nonceSym, err := tss.SignComplain(
+		suite.member1.OneTimePubKey,
+		suite.member2.OneTimePubKey,
+		suite.member1.OneTimePrivKey,
+	)
 	suite.Require().NoError(err)
 
-	challenge := tss.GenerateMessageComplain(suite.kpI.PublicKey, suite.kpJ.PublicKey, keySym)
+	// Success case
+	err = tss.VerifyComplainSig(suite.member1.OneTimePubKey, suite.member2.OneTimePubKey, keySym, nonceSym, sig)
+	suite.Require().NoError(err)
+
+	// Wrong public key I case
+	err = tss.VerifyComplainSig(suite.fakeKey.PublicKey, suite.member2.OneTimePubKey, keySym, nonceSym, sig)
+	suite.Require().Error(err)
+
+	// Wrong public key J case
+	err = tss.VerifyComplainSig(suite.member1.OneTimePubKey, suite.fakeKey.PublicKey, keySym, nonceSym, sig)
+	suite.Require().Error(err)
+
+	// Wrong key sym case
+	err = tss.VerifyComplainSig(
+		suite.member1.OneTimePubKey,
+		suite.member2.OneTimePubKey,
+		suite.fakeKey.PublicKey,
+		nonceSym,
+		sig,
+	)
+	suite.Require().Error(err)
+
+	// Wrong nonce sym case
+	err = tss.VerifyComplainSig(
+		suite.member1.OneTimePubKey,
+		suite.member2.OneTimePubKey,
+		keySym,
+		suite.fakeKey.PublicKey,
+		sig,
+	)
+	suite.Require().Error(err)
+}
+
+func (suite *TSSTestSuite) TestGenerateMessageComplain() {
+	keySym, err := tss.ComputeKeySym(suite.member1.OneTimePrivKey, suite.member2.OneTimePubKey)
+	suite.Require().NoError(err)
+
+	challenge := tss.GenerateMessageComplain(suite.member1.OneTimePubKey, suite.member2.OneTimePubKey, keySym)
 	suite.Require().Equal(
-		"726f756e6433436f6d706c61696e03f70e80bac0b32b2599fa54d83b5471e90fac27bb09528f0337b49d464d64426f03f70e80bac0b32b2599fa54d83b5471e90fac27bb09528f0337b49d464d64426f03bc213e4251592d29c070e4c31b980d150e755ec848afa4c06730ec1dcd09c482",
+		"726f756e6433436f6d706c61696e02e20b4d6bd3f10e7c3a9098c5832180b809a826ae49a972d5348758529c5015c502e20b4d6bd3f10e7c3a9098c5832180b809a826ae49a972d5348758529c5015c5035db2a125a23300bef24e57883f547503ab2598a99ed07d65d482b4ea1ff8ed26",
 		hex.EncodeToString(challenge),
 	)
 }
--- pkg/tss/signing_test.go
+++ pkg/tss/signing_test.go
@@ -1,13 +1,79 @@
 package tss_test
 
 import (
+	"encoding/hex"
+
 	"github.com/bandprotocol/chain/v2/pkg/tss"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 )
 
 func (suite *TSSTestSuite) TestComputeLagrangeCoefficient() {
 	expValue := tss.ParseScalar(new(secp256k1.ModNScalar).SetInt(1140))
-	value := tss.ComputeLagrangeCoefficient(3, 20)
+	value := tss.ComputeLagrangeCoefficient(
+		tss.MemberID(2),
+		[]tss.MemberID{1, 2},
+	)
 
-	suite.Require().Equal(expValue, value)
+	suite.Require().Equal(expValue, hex.EncodeToString(value))
+}
+
+func (suite *TSSTestSuite) TestSignAndVerifySigning() {
+	// Sign
+	sig, err := tss.SignSigning(
+		suite.groupPubNonce,
+		suite.groupPubKey,
+		suite.data,
+		suite.member1.lagrange,
+		suite.member1.ownNonce.PrivateKey,
+		suite.member1.ownKey.PrivateKey,
+	)
+	suite.Require().NoError(err)
+
+	// Success case
+	err = tss.VerifySigningSig(
+		suite.groupPubNonce,
+		suite.groupPubKey,
+		suite.data,
+		suite.member1.lagrange,
+		sig,
+		suite.member1.ownKey.PublicKey,
+	)
+	suite.Require().NoError(err)
+}
+
+func (suite *TSSTestSuite) TestSignAndVerifyGroupSigning() {
+	// Sign by member1
+	sig1, err := tss.SignSigning(
+		suite.groupPubNonce,
+		suite.groupPubKey,
+		suite.data,
+		suite.member1.lagrange,
+		suite.member1.ownNonce.PrivateKey,
+		suite.member1.ownKey.PrivateKey,
+	)
+	suite.Require().NoError(err)
+
+	// Sign by member2
+	sig2, err := tss.SignSigning(
+		suite.groupPubNonce,
+		suite.groupPubKey,
+		suite.data,
+		suite.member2.lagrange,
+		suite.member2.ownNonce.PrivateKey,
+		suite.member2.ownKey.PrivateKey,
+	)
+	suite.Require().NoError(err)
+
+	sig, err := tss.CombineSignatures(sig1, sig2)
+	suite.Require().Equal(suite.groupPubNonce, tss.PublicKey(sig.R()))
+
+	// Success case
+	err = tss.VerifyGroupSigningSig(suite.groupPubKey, suite.data, sig)
+	suite.Require().NoError(err)
+}
+
+func (suite *TSSTestSuite) TestGenerateMessageGroupSigning() {
+	msg := tss.GenerateMessageGroupSigning(suite.groupPubKey, suite.data)
+	suite.Require().
+		Equal("7369676e696e6703534dfb533fedd09a97cbedeab70ae895399ed48be0ad7f789a705ec023dcf04464617461", hex.EncodeToString(msg))
 }
