852fdaab521ddd2711f6f8512820d6894b9c7eea
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2023-05-30 16:39:53 +0700

    fixed type

--- cylinder/client/client.go
+++ cylinder/client/client.go
@@ -145,7 +145,7 @@ func (c *Client) BroadcastAndConfirm(
 		)
 		time.Sleep(c.pollInterval)
 		if err != nil {
-			logger.Debug(":anxious_face_with_sweat: Try %d: Failed to broadcast msgs with error: %s", err.Error(), try)
+			logger.Debug(":anxious_face_with_sweat: Try %d: Failed to broadcast msgs with error: %s", try, err.Error())
 			continue
 		}
 
@@ -155,8 +155,8 @@ func (c *Client) BroadcastAndConfirm(
 			if err != nil {
 				logger.Debug(
 					":anxious_face_with_sweat: Try %d: Failed to get tx from hash with error: %s",
-					err.Error(),
 					try,
+					err.Error(),
 				)
 				continue
 			}
@@ -169,14 +169,15 @@ func (c *Client) BroadcastAndConfirm(
 		if res.Codespace == sdkerrors.RootCodespace && res.Code == sdkerrors.ErrOutOfGas.ABCICode() {
 			gasAdjust += c.gasAdjustStep
 			logger.Debug(
-				":anxious_face_with_sweat: Try %d: Bumping gas since tx is out of gas: new gad adjustment %d",
+				":anxious_face_with_sweat: Try %d: Bumping gas since tx is out of gas: new gad adjustment %f",
 				try,
 				gasAdjust,
 			)
 		}
 
 		logger.Debug(
-			":anxious_face_with_sweat: Try %d: Transaction is not successful with error code: codespace: %s, code: %d",
+			":anxious_face_with_sweat: Try %d: Transaction is not successful with error code: codespace: %d, code: %d",
+			try,
 			res.Codespace,
 			res.Code,
 		)
