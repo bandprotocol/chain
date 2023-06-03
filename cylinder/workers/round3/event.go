14c15d93c413f3eafdee00b2ac17f82badf30923
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2023-06-04 01:40:55 +0700

    update

--- cylinder/workers/round3/event.go
+++ cylinder/workers/round3/event.go
@@ -1,8 +1,6 @@
 package round3
 
 import (
-	"strconv"
-
 	"github.com/bandprotocol/chain/v2/pkg/event"
 	"github.com/bandprotocol/chain/v2/pkg/tss"
 	"github.com/bandprotocol/chain/v2/x/tss/types"
@@ -17,12 +15,7 @@ type Event struct {
 // ParseEvent parses the round3 event from the given message log.
 // It extracts the group ID from the log and returns the parsed Event or an error if parsing fails.
 func ParseEvent(log sdk.ABCIMessageLog) (*Event, error) {
-	gidStr, err := event.GetEventValue(log, types.EventTypeRound2Success, types.AttributeKeyGroupID)
-	if err != nil {
-		return nil, err
-	}
-
-	gid, err := strconv.ParseUint(gidStr, 10, 64)
+	gid, err := event.GetEventValueUint64(log, types.EventTypeRound2Success, types.AttributeKeyGroupID)
 	if err != nil {
 		return nil, err
 	}
