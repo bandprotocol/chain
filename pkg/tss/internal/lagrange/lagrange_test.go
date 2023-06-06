8dfc0da95db4971b066340c4f587ff28f122e672
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2023-06-06 19:15:18 +0700

    use set string instead

--- pkg/tss/internal/lagrange/lagrange_test.go
+++ pkg/tss/internal/lagrange/lagrange_test.go
@@ -11,51 +11,50 @@ import (
 func TestComputeCoefficient(t *testing.T) {
 	tests := []struct {
 		name string
-		i    int64    // test description
-		s    []int64  // hex encoded signature to parse
-		exp  *big.Int // expected error
+		i    int64   // test description
+		s    []int64 // hex encoded signature to parse
+		exp  string
 	}{{
 		name: "1 from 1,2",
 		i:    1,
 		s:    []int64{1, 2},
-		exp:  big.NewInt(2),
+		exp:  "2",
 	}, {
 		name: "2 from 1,2",
 		i:    2,
 		s:    []int64{1, 2},
-		exp: big.NewInt(0).
-			SetBits([]big.Word{13822214165235122496, 13451932020343611451, 18446744073709551614, 18446744073709551615}),
+		exp:  "115792089237316195423570985008687907852837564279074904382605163141518161494336",
 	}, {
 		name: "1 from 1,2,3",
 		i:    1,
 		s:    []int64{1, 2, 3},
-		exp:  big.NewInt(3),
+		exp:  "3",
 	}, {
 		name: "2 from 1,2,4",
 		i:    2,
 		s:    []int64{1, 2, 4},
-		exp: big.NewInt(0).
-			SetBits([]big.Word{13822214165235122495, 13451932020343611451, 18446744073709551614, 18446744073709551615}),
+		exp:  "115792089237316195423570985008687907852837564279074904382605163141518161494335",
 	}, {
 		name: "3 from 1,3,4",
 		i:    3,
 		s:    []int64{1, 3, 4},
-		exp: big.NewInt(0).
-			SetBits([]big.Word{13822214165235122495, 13451932020343611451, 18446744073709551614, 18446744073709551615}),
+		exp:  "115792089237316195423570985008687907852837564279074904382605163141518161494335",
 	}, {
 		name: "4 from 2,3,4",
 		i:    4,
 		s:    []int64{2, 3, 4},
-		exp:  big.NewInt(3),
+		exp:  "3",
 	}}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			res := lagrange.ComputeCoefficient(test.i, test.s)
-			assert.Equal(t, test.exp, res)
+			exp, _ := new(big.Int).SetString(test.exp, 10)
+
+			assert.Equal(t, exp, res)
 
 			res = lagrange.ComputeCoefficientOptimize(test.i, test.s)
-			assert.Equal(t, test.exp, res)
+			assert.Equal(t, exp, res)
 		})
 	}
 }
