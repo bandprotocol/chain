cd23059b7a40f4f7a2bca61ae453c958b9d09fa0
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2023-06-12 18:48:53 +0700

    add account for tss

--- scripts/generate_genesis.sh
+++ scripts/generate_genesis.sh
@@ -8,12 +8,20 @@ echo "lock nasty suffer dirt dream fine fall deal curtain plate husband sound to
     | bandd keys add validator --recover --keyring-backend test
 echo "smile stem oven genius cave resource better lunar nasty moon company ridge brass rather supply used horn three panic put venue analyst leader comic" \
     | bandd keys add requester --recover --keyring-backend test
+echo "drop video mention casual soldier ostrich resemble harvest casual step design gasp grunt lab meadow buzz envelope today spy cliff column habit fall eyebrow" \
+    | bandd keys add tss1 --recover --keyring-backend test
+echo "enlist electric thumb valve inherit visa ecology trust cake argue forward hidden thing analyst science treat ice lend pumpkin today ticket purchase process pioneer" \
+    | bandd keys add tss2 --recover --keyring-backend test
+echo "measure fence mail fluid olive cute empower fossil ahead manage snow marble dash citizen tourist skate assist solution bonus spend tip negative try eyebrow" \
+    | bandd keys add tss3 --recover --keyring-backend test
 
 
 # add accounts to genesis
 bandd add-genesis-account validator 10000000000000uband --keyring-backend test
 bandd add-genesis-account requester 10000000000000uband --keyring-backend test
-
+bandd add-genesis-account tss1 10000000000000uband --keyring-backend test
+bandd add-genesis-account tss2 10000000000000uband --keyring-backend test
+bandd add-genesis-account tss3 10000000000000uband --keyring-backend test
 
 # register initial validators
 bandd gentx validator 100000000uband \
