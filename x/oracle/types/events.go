74e65c9060e0a4f4e027e8a4aa714872f286256d
satawatnack <s.nack1919@gmail.com>
2023-06-26 13:54:22 +0700

    reorder attributes

--- x/oracle/types/events.go
+++ x/oracle/types/events.go
@@ -18,8 +18,8 @@ const (
 	EventTypeTSSHandleRequestSignFail = "tss_handle_request_sign_fail"
 
 	AttributeKeyID             = "id"
-	AttributeKeyTSSSigningID   = "tss_signing_id"
 	AttributeKeyTSSGroupID     = "tss_group_id"
+	AttributeKeyTSSSigningID   = "tss_signing_id"
 	AttributeKeyDataSourceID   = "data_source_id"
 	AttributeKeyOracleScriptID = "oracle_script_id"
 	AttributeKeyExternalID     = "external_id"
