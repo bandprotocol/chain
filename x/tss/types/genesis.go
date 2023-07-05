4c2d126201e897d94fd02499bf44185ced5a4798
satawatnack <s.nack1919@gmail.com>
2023-07-05 17:40:49 +0700

    add default genesis

--- x/tss/types/genesis.go
+++ x/tss/types/genesis.go
@@ -1,13 +1,24 @@
 package types
 
 // NewGenesisState - Create a new genesis state
-func NewGenesisState(params Params) *GenesisState {
+func NewGenesisState(
+	params Params,
+	groupCount uint64,
+	signingCount uint64,
+	groups []Group,
+	deQueuesGenesis []DEQueueGenesis,
+	desGenesis []DEGenesis,
+) *GenesisState {
 	return &GenesisState{
-		Params: params,
+		Params:          params,
+		GroupCount:      groupCount,
+		SigningCount:    signingCount,
+		DEQueuesGenesis: deQueuesGenesis,
+		DEsGenesis:      desGenesis,
 	}
 }
 
 // DefaultGenesisState returns the default tss genesis state.
 func DefaultGenesisState() *GenesisState {
-	return NewGenesisState(DefaultParams())
+	return NewGenesisState(DefaultParams(), 0, 0, []Group{}, []DEQueueGenesis{}, []DEGenesis{})
 }
