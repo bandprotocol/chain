03ab004257f1e5378775b7c1fc74629e1aab4bdc
satawatnack <satawat@bandprotocol.com>
2023-10-09 11:51:41 +0700

    add necessary events for emitter

--- x/tss/keeper/msg_server.go
+++ x/tss/keeper/msg_server.go
@@ -190,6 +190,13 @@ func (k msgServer) ReplaceGroup(
 	toGroup.LatestReplacementID = nextID
 	k.SetGroup(ctx, toGroup)
 
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			types.EventTypeReplaceGroup,
+			sdk.NewAttribute(types.AttributeKeyReplaceGroupID, fmt.Sprintf("%d", nextID)),
+		),
+	)
+
 	return &types.MsgReplaceGroupResponse{}, nil
 }
 
@@ -215,6 +222,14 @@ func (k msgServer) UpdateGroupFee(
 	// Set new group fee
 	group.Fee = req.Fee.Sort()
 	k.SetGroup(ctx, group)
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			types.EventTypeUpdateGroupFee,
+			sdk.NewAttribute(types.AttributeKeyGroupID, fmt.Sprintf("%d", group.GroupID)),
+			sdk.NewAttribute(types.AttributeKeyFee, group.Fee.String()),
+		),
+	)
 
 	return &types.MsgUpdateGroupFeeResponse{}, nil
 }
@@ -766,7 +781,7 @@ func (k msgServer) Activate(goCtx context.Context, msg *types.MsgActivate) (*typ
 
 	ctx.EventManager().EmitEvent(sdk.NewEvent(
 		types.EventTypeActivate,
-		sdk.NewAttribute(types.AttributeKeyMember, msg.Address),
+		sdk.NewAttribute(types.AttributeKeyAddress, msg.Address),
 	))
 
 	return &types.MsgActivateResponse{}, nil
@@ -787,7 +802,7 @@ func (k msgServer) Active(goCtx context.Context, msg *types.MsgActive) (*types.M
 
 	ctx.EventManager().EmitEvent(sdk.NewEvent(
 		types.EventTypeActive,
-		sdk.NewAttribute(types.AttributeKeyMember, msg.Address),
+		sdk.NewAttribute(types.AttributeKeyAddress, msg.Address),
 	))
 
 	return &types.MsgActiveResponse{}, nil
