5bb08471574f24550cde4f586b587dcb3356b2e6
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2023-10-10 15:28:58 +0700

    fix proof path

--- client/grpc/oracle/proof/multi_store.go
+++ client/grpc/oracle/proof/multi_store.go
@@ -8,33 +8,33 @@ import (
 
 // MultiStoreProofEthereum is an Ethereum version of MultiStoreProof for solidity ABI-encoding.
 type MultiStoreProofEthereum struct {
-	OracleIAVLStateHash              common.Hash
-	MintStoreMerkleHash              common.Hash
-	ParamsToSlashingStoresMerkleHash common.Hash
-	GovToIcahostStoresMerkleHash     common.Hash
-	AuthToFeegrantStoresMerkleHash   common.Hash
-	StakingToUpgradeStoresMerkleHash common.Hash
+	OracleIAVLStateHash             common.Hash
+	MintStoreMerkleHash             common.Hash
+	IbcToIcahostStoresMerkleHash    common.Hash
+	FeegrantToGroupStoresMerkleHash common.Hash
+	AuthToEvidenceStoresMerkleHash  common.Hash
+	ParamsToUpgradeStoresMerkleHash common.Hash
 }
 
 func (m *MultiStoreProof) encodeToEthFormat() MultiStoreProofEthereum {
 	return MultiStoreProofEthereum{
-		OracleIAVLStateHash:              common.BytesToHash(m.OracleIAVLStateHash),
-		MintStoreMerkleHash:              common.BytesToHash(m.MintStoreMerkleHash),
-		ParamsToSlashingStoresMerkleHash: common.BytesToHash(m.ParamsToSlashingStoresMerkleHash),
-		GovToIcahostStoresMerkleHash:     common.BytesToHash(m.GovToIcahostStoresMerkleHash),
-		AuthToFeegrantStoresMerkleHash:   common.BytesToHash(m.AuthToFeegrantStoresMerkleHash),
-		StakingToUpgradeStoresMerkleHash: common.BytesToHash(m.StakingToUpgradeStoresMerkleHash),
+		OracleIAVLStateHash:             common.BytesToHash(m.OracleIAVLStateHash),
+		MintStoreMerkleHash:             common.BytesToHash(m.MintStoreMerkleHash),
+		IbcToIcahostStoresMerkleHash:    common.BytesToHash(m.IbcToIcahostStoresMerkleHash),
+		FeegrantToGroupStoresMerkleHash: common.BytesToHash(m.FeegrantToGroupStoresMerkleHash),
+		AuthToEvidenceStoresMerkleHash:  common.BytesToHash(m.AuthToEvidenceStoresMerkleHash),
+		ParamsToUpgradeStoresMerkleHash: common.BytesToHash(m.ParamsToUpgradeStoresMerkleHash),
 	}
 }
 
 // GetMultiStoreProof compacts Multi store proof from Tendermint to MultiStoreProof version.
 func GetMultiStoreProof(multiStoreEp *ics23.ExistenceProof) MultiStoreProof {
 	return MultiStoreProof{
-		OracleIAVLStateHash:              tmbytes.HexBytes(multiStoreEp.Value),
-		MintStoreMerkleHash:              tmbytes.HexBytes(multiStoreEp.Path[0].Prefix[1:]),
-		ParamsToSlashingStoresMerkleHash: tmbytes.HexBytes(multiStoreEp.Path[1].Suffix),
-		GovToIcahostStoresMerkleHash:     tmbytes.HexBytes(multiStoreEp.Path[2].Prefix[1:]),
-		AuthToFeegrantStoresMerkleHash:   tmbytes.HexBytes(multiStoreEp.Path[3].Prefix[1:]),
-		StakingToUpgradeStoresMerkleHash: tmbytes.HexBytes(multiStoreEp.Path[4].Suffix),
+		OracleIAVLStateHash:             tmbytes.HexBytes(multiStoreEp.Value),
+		MintStoreMerkleHash:             tmbytes.HexBytes(multiStoreEp.Path[0].Prefix[1:]),
+		IbcToIcahostStoresMerkleHash:    tmbytes.HexBytes(multiStoreEp.Path[1].Prefix[1:]),
+		FeegrantToGroupStoresMerkleHash: tmbytes.HexBytes(multiStoreEp.Path[2].Prefix[1:]),
+		AuthToEvidenceStoresMerkleHash:  tmbytes.HexBytes(multiStoreEp.Path[3].Prefix[1:]),
+		ParamsToUpgradeStoresMerkleHash: tmbytes.HexBytes(multiStoreEp.Path[4].Suffix),
 	}
 }
