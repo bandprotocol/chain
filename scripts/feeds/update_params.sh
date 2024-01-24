5fd811be7ff318c25bf12ddff65e4544231e07d4
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2024-01-24 16:49:41 +0700

    update script

--- scripts/feeds/update_params.sh
+++ scripts/feeds/update_params.sh
@@ -1,22 +1,25 @@
 
 #!/bin/bash
 
-export WALLET_NAME=validator
+export WALLET_NAME=validator1
+export ENDPOINT=http://localhost:26657
 
 BASEDIR=$(dirname "$0")
 
 # Submit create_group proposal
 bandd tx gov submit-proposal $BASEDIR/proposal_update_params.json \
   --from $WALLET_NAME \
+  --node $ENDPOINT \
   -y --keyring-backend test --gas-prices 0.0025uband -b sync
 
 sleep 5
 
 # Query last proposal's id
-PROPOSAL_ID=$(bandd query gov proposals --reverse --limit 1 --output json | jq -r '.proposals[0].id')
+PROPOSAL_ID=$(bandd query gov proposals --node $ENDPOINT --reverse --limit 1 --output json | jq -r '.proposals[0].id')
 
 # Vote on that proposal
 echo "...Voting to proposal $PROPOSAL_ID..."
 bandd tx gov vote $PROPOSAL_ID yes \
   --from $WALLET_NAME \
+  --node $ENDPOINT \
   -y --keyring-backend test --gas-prices 0.0025uband -b sync
