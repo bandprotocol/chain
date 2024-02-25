4cc54f0f454bc3071eb2d44df6f40eda33e6a8c9
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2024-02-25 21:41:26 +0700

    fix name

--- x/feeds/types/errors.go
+++ x/feeds/types/errors.go
@@ -1,22 +1,22 @@
 package types
 
 import (
-	"cosmossdk.io/errors"
+	errorsmod "cosmossdk.io/errors"
 )
 
 // x/feeds module sentinel errors
 var (
-	ErrPriceNotFound           = errors.Register(ModuleName, 2, "price not found")
-	ErrPriceValidatorNotFound  = errors.Register(ModuleName, 3, "price validator not found")
-	ErrSymbolNotFound          = errors.Register(ModuleName, 4, "symbol not found")
-	ErrPriceServiceNotFound    = errors.Register(ModuleName, 5, "price-service not found")
-	ErrOracleStatusNotActive   = errors.Register(ModuleName, 6, "oracle status not active")
-	ErrPriceTooFast            = errors.Register(ModuleName, 7, "price is too fast")
-	ErrInvalidTimestamp        = errors.Register(ModuleName, 8, "invalid timestamp")
-	ErrNotEnoughPriceValidator = errors.Register(ModuleName, 9, "not enough price validator")
-	ErrInvalidSigner           = errors.Register(ModuleName, 10, "expected admin to be signer")
-	ErrNotTopValidator         = errors.Register(ModuleName, 11, "not top validator")
-	ErrNotEnoughDelegation     = errors.Register(ModuleName, 12, "not enough delegation")
-	ErrSymbolPowerNotFound     = errors.Register(ModuleName, 13, "symbol power not found")
-	ErrUnableToUndelegate      = errors.Register(ModuleName, 14, "unable to undelegate")
+	ErrPriceNotFound           = errorsmod.Register(ModuleName, 2, "price not found")
+	ErrPriceValidatorNotFound  = errorsmod.Register(ModuleName, 3, "price validator not found")
+	ErrSymbolNotFound          = errorsmod.Register(ModuleName, 4, "symbol not found")
+	ErrPriceServiceNotFound    = errorsmod.Register(ModuleName, 5, "price-service not found")
+	ErrOracleStatusNotActive   = errorsmod.Register(ModuleName, 6, "oracle status not active")
+	ErrPriceTooFast            = errorsmod.Register(ModuleName, 7, "price is too fast")
+	ErrInvalidTimestamp        = errorsmod.Register(ModuleName, 8, "invalid timestamp")
+	ErrNotEnoughPriceValidator = errorsmod.Register(ModuleName, 9, "not enough price validator")
+	ErrInvalidSigner           = errorsmod.Register(ModuleName, 10, "expected admin to be signer")
+	ErrNotTopValidator         = errorsmod.Register(ModuleName, 11, "not top validator")
+	ErrNotEnoughDelegation     = errorsmod.Register(ModuleName, 12, "not enough delegation")
+	ErrSymbolPowerNotFound     = errorsmod.Register(ModuleName, 13, "symbol power not found")
+	ErrUnableToUndelegate      = errorsmod.Register(ModuleName, 14, "unable to undelegate")
 )
