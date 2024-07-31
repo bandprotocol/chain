cd89e66783fb8c01e83f12f35bab3fb09de99093
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2024-07-31 18:11:19 +0700

    fix format

--- x/feeds/abci.go
+++ x/feeds/abci.go
@@ -8,7 +8,10 @@ import (
 
 // HandleEndBlock is a handler function for the EndBlock ABCI request.
 func HandleEndBlock(ctx sdk.Context, k keeper.Keeper) {
+	// re-calculate prices of all current feeds
 	k.CalculatePrices(ctx)
+
+	// re-calculate current feeds every `CurrentFeedsUpdateInterval` blocks
 	if ctx.BlockHeight()%k.GetParams(ctx).CurrentFeedsUpdateInterval == 0 {
 		feeds := k.CalculateNewCurrentFeeds(ctx)
 		k.SetCurrentFeeds(ctx, feeds)
--- x/feeds/types/validate.go
+++ x/feeds/types/validate.go
@@ -11,9 +11,11 @@ func validateInt64(name string, positiveOnly bool, i interface{}) error {
 	if !ok {
 		return fmt.Errorf("invalid parameter type: %T", i)
 	}
+
 	if v <= 0 && positiveOnly {
 		return fmt.Errorf("%s must be positive: %d", name, v)
 	}
+
 	return nil
 }
 
@@ -23,9 +25,11 @@ func validateUint64(name string, positiveOnly bool, i interface{}) error {
 	if !ok {
 		return fmt.Errorf("invalid parameter type: %T", i)
 	}
+
 	if v == 0 && positiveOnly {
 		return fmt.Errorf("%s must be positive: %d", name, v)
 	}
+
 	return nil
 }
 
@@ -35,6 +39,7 @@ func validateURL(name string, u string) error {
 	if err != nil {
 		return fmt.Errorf("%s has invalid URL format", name)
 	}
+
 	return nil
 }
 
@@ -44,8 +49,10 @@ func validateString(name string, allowEmpty bool, i interface{}) error {
 	if !ok {
 		return fmt.Errorf("invalid parameter type: %T", i)
 	}
+
 	if s == "" && !allowEmpty {
 		return fmt.Errorf("%s cannot be empty", name)
 	}
+
 	return nil
 }
