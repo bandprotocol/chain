e7af15801b269f4e536382e8bbd6fc341adfd759
satawatnack <satawat@bandprotocol.com>
2024-09-18 14:11:57 +0700

    update validate init deposit

--- x/tunnel/types/msgs.go
+++ x/tunnel/types/msgs.go
@@ -121,7 +121,7 @@ func (m MsgCreateTunnel) ValidateBasic() error {
 	}
 
 	// minimum deposit must be positive
-	if !m.Deposit.IsValid() {
+	if !m.Deposit.IsValid() || m.Deposit.IsAllPositive() {
 		return sdkerrors.ErrInvalidCoins.Wrapf("invalid deposit: %s", m.Deposit)
 	}
 
