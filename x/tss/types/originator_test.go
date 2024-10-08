6f14d06b67f83eec7b235a44c404bf5f622d1c7f
nkitlabs <natthakunk0109@gmail.com>
2024-10-08 09:08:07 +0700

    add originitor test

---
+++ x/tss/types/originator_test.go
@@ -0,0 +1,67 @@
+package types_test
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/bandprotocol/chain/v2/x/tss/types"
+)
+
+func TestEncodeTunnelOriginator(t *testing.T) {
+	// Define test cases
+	testCases := []struct {
+		name       string
+		originator types.TunnelOriginator
+		expected   string
+		err        error
+	}{
+		{
+			name: "EVM chain",
+			originator: types.TunnelOriginator{
+				TunnelID:        256,
+				ContractAddress: "0x5662ac531A2737C3dB8901E982B43327a2fDe2ae",
+				ChainID:         "BSC",
+			},
+			expected: "a466d313088002122a3078353636326163353331413237333743336442383930314539383242343333323761326644653261651a03425343",
+		},
+	}
+
+	// Run the test cases
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := tc.originator.Encode()
+			require.Equal(t, tc.err, err)
+			require.Equal(t, tc.expected, hex.EncodeToString(result))
+		})
+	}
+}
+
+func TestEncodeDirectOriginator(t *testing.T) {
+	// Define test cases
+	testCases := []struct {
+		name       string
+		originator types.DirectOriginator
+		expected   string
+		err        error
+	}{
+		{
+			name: "Normal",
+			originator: types.DirectOriginator{
+				Requester: "band10d07y265gmmuvt4z0w9aw880jnsr700jrdn8wm",
+				Memo:      "test",
+			},
+			expected: "3c839c260a2b62616e64313064303779323635676d6d757674347a30773961773838306a6e73723730306a72646e38776d120474657374",
+		},
+	}
+
+	// Run the test cases
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := tc.originator.Encode()
+			require.Equal(t, tc.err, err)
+			require.Equal(t, tc.expected, hex.EncodeToString(result))
+		})
+	}
+}
