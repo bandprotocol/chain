a5c0e9319458662f013c6827351a4b5a7ac6d155
colmazia <ongart.pisan@gmail.com>
2024-10-11 14:09:26 +0700

    fix lint

--- cmd/grogu/cmd/run.go
+++ cmd/grogu/cmd/run.go
@@ -12,9 +12,9 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
-	"cosmossdk.io/log"
-
 	dbm "github.com/cosmos/cosmos-db"
+
+	"cosmossdk.io/log"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
