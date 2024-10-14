5b22cd8a48ca5f6255a2a16d771fa59185a844c3
Kitipong Sirirueangsakul <kitipong.sirir@gmail.com>
2024-10-15 02:35:43 +0700

    add db

---
+++ flusher/flusher/restake_db.py
@@ -0,0 +1,30 @@
+import sqlalchemy as sa
+
+from .db import (
+    metadata,
+    Column,
+)
+
+restake_vaults = sa.Table(
+    "restake_vaults",
+    metadata,
+    Column("key", sa.String, primary_key=True),
+    Column("vault_id", sa.Integer, sa.ForeignKey("accounts.id")),
+    Column("is_active", sa.Boolean),
+    Column("total_power", sa.BigInteger),
+)
+
+restake_locks = sa.Table(
+    "restake_locks",
+    metadata,
+    Column("account_id", sa.Integer, sa.ForeignKey("accounts.id"), primary_key=True),
+    Column("key", sa.String, primary_key=True),
+    Column("power", sa.BigInteger),
+)
+
+restake_stakes = sa.Table(
+    "restake_locks",
+    metadata,
+    Column("account_id", sa.Integer, sa.ForeignKey("accounts.id"), primary_key=True),
+    Column("coins", sa.String),
+)
