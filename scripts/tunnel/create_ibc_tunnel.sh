3b47a7fee87dd1ac3f0ed510d7781975c00d14c5
satawatnack <satawat@bandprotocol.com>
2024-10-21 15:51:48 +0700

    add script

---
+++ scripts/tunnel/create_ibc_tunnel.sh
@@ -0,0 +1,1 @@
+bandd tx tunnel create-tunnel ibc channel-0 1 1uband 10 ./scripts/tunnel/signal_deviations.json --from requester --keyring-backend test --gas-prices 0.0025uband -y --chain-id bandchain
