66b14c7d9efea91deb62b6c90dba4d1c5ce34524
colmazia <ongart.pisan@gmail.com>
2024-10-31 14:48:40 +0700

    remove module codec

--- x/restake/types/codec.go
+++ x/restake/types/codec.go
@@ -8,13 +8,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
-// ModuleCdc references the global x/restake module codec. Note, the codec
-// should ONLY be used in certain instances of tests and for JSON encoding.
-//
-// The actual codec used for serialization should be provided to x/restake and
-// defined at the application level.
-var ModuleCdc = codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
-
 // RegisterLegacyAminoCodec registers the necessary x/restake interfaces and concrete types
 // on the provided LegacyAmino codec. These types are used for Amino JSON serialization.
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
