c857d80b86572243247737d763204f67d685e6a6
nkitlabs <natthakunk0109@gmail.com>
2024-11-07 17:29:54 +0700

    move to helper file

--- x/tunnel/keeper/packet.go
+++ x/tunnel/keeper/packet.go
@@ -111,7 +111,7 @@ func (k Keeper) ProducePacket(
 	sendAll := unixNow >= int64(tunnel.Interval)+latestSignalPrices.LastInterval
 
 	// generate new signal prices; if no new signal prices, stop the process.
-	newSignalPrices, err := k.GenerateNewSignalPrices(
+	newSignalPrices, err := GenerateNewSignalPrices(
 		latestSignalPrices,
 		tunnel.GetSignalDeviationMap(),
 		currentPricesMap,
@@ -212,54 +212,6 @@ func (k Keeper) SendPacket(ctx sdk.Context, packet types.Packet) error {
 	k.SetPacket(ctx, packet)
 
 	return nil
-}
-
-// GenerateNewSignalPrices generates new signal prices based on the current prices
-// and signal deviations.
-func (k Keeper) GenerateNewSignalPrices(
-	latestSignalPrices types.LatestSignalPrices,
-	signalDeviationsMap map[string]types.SignalDeviation,
-	currentFeedsPricesMap map[string]feedstypes.Price,
-	sendAll bool,
-) ([]types.SignalPrice, error) {
-	shouldSend := false
-	newSignalPrices := make([]types.SignalPrice, 0)
-	for _, sp := range latestSignalPrices.SignalPrices {
-		oldPrice := sdkmath.NewIntFromUint64(sp.Price)
-
-		// get current price from the feed, if not found, set price to 0
-		price := uint64(0)
-		feedPrice, ok := currentFeedsPricesMap[sp.SignalID]
-		if ok && feedPrice.PriceStatus == feedstypes.PriceStatusAvailable {
-			price = feedPrice.Price
-		}
-		newPrice := sdkmath.NewIntFromUint64(price)
-
-		// get hard/soft deviation, panic if not found; should not happen.
-		sd, ok := signalDeviationsMap[sp.SignalID]
-		if !ok {
-			panic(fmt.Sprintf("deviation not found for signal ID: %s", sp.SignalID))
-		}
-		hardDeviation := sdkmath.NewIntFromUint64(sd.HardDeviationBPS)
-		softDeviation := sdkmath.NewIntFromUint64(sd.SoftDeviationBPS)
-
-		// calculate deviation between old price and new price and compare with the threshold.
-		// shouldSend is set to true if sendAll is true or there is a signal whose deviation
-		// is over the hard threshold.
-		deviation := calculateDeviationBPS(oldPrice, newPrice)
-		if sendAll || deviation.GTE(hardDeviation) {
-			newSignalPrices = append(newSignalPrices, types.NewSignalPrice(sp.SignalID, price))
-			shouldSend = true
-		} else if deviation.GTE(softDeviation) {
-			newSignalPrices = append(newSignalPrices, types.NewSignalPrice(sp.SignalID, price))
-		}
-	}
-
-	if shouldSend {
-		return newSignalPrices, nil
-	} else {
-		return []types.SignalPrice{}, nil
-	}
 }
 
 // calculateDeviationBPS calculates the deviation between the old price and
