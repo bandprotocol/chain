ad15b80c283319bebd598e56b1dc74436819acdb
satawatnack <satawat@bandprotocol.com>
2024-11-28 15:41:25 +0700

    .

--- x/tunnel/types/query.pb.gw.go
+++ x/tunnel/types/query.pb.gw.go
@@ -862,7 +862,7 @@ var (
 
 	pattern_Query_Packet_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4, 1, 0, 4, 1, 5, 5}, []string{"tunnel", "v1beta1", "tunnels", "tunnel_id", "packets", "sequence"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_TotalFees_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"tunnel", "v1beta1", "total_fee"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_TotalFees_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"tunnel", "v1beta1", "total_fees"}, "", runtime.AssumeColonVerbOpt(false)))
 
 	pattern_Query_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"tunnel", "v1beta1", "params"}, "", runtime.AssumeColonVerbOpt(false)))
 )
