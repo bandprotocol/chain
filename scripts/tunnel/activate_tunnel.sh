0e02837fe4a4f78d300b62b83fc257c2eef8017e
satawatnack <satawat@bandprotocol.com>
2025-01-06 13:05:52 +0700

    .

--- scripts/tunnel/activate_tunnel.sh
+++ scripts/tunnel/activate_tunnel.sh
@@ -1,1 +1,1 @@
-bandd tx tunnel activate-tunnel 1 --from requester --keyring-backend test --gas-prices 0.0025uband -y --chain-id bandchain
+bandd tx tunnel activate 1 --from requester --keyring-backend test --gas-prices 0.0025uband -y --chain-id bandchain
