46ee44fb03d5c53c47a0bf3ec3ca403c1c2b6a89
satawatnack <satawat@bandprotocol.com>
2025-02-03 16:34:26 +0700

    update create ibc hook tunnel script

--- scripts/tunnel/create_ibc_hook_tunnel.sh
+++ scripts/tunnel/create_ibc_hook_tunnel.sh
@@ -1,1 +1,1 @@
-bandd tx tunnel create-tunnel ibc-hook channel-0 wasm1vjq0k3fj47s8wns4a7zw5c4lsjd8l6r2kzzlpk 1 1uband 10 ./scripts/tunnel/signal_deviations.json --from requester --keyring-backend test --gas-prices 0.0025uband -y --chain-id bandchain
+bandd tx tunnel create-tunnel ibc-hook channel-0 wasm1vjq0k3fj47s8wns4a7zw5c4lsjd8l6r2kzzlpk 1uband 60 ./scripts/tunnel/signal_deviations.json --from requester --keyring-backend test --gas-prices 0.0025uband -y --chain-id bandchain
