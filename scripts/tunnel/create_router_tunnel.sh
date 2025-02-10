dde6bcb68d74a2ac052eb2204f5e84db395fed58
satawatnack <satawat@bandprotocol.com>
2025-02-10 11:33:23 +0700

    update cli

--- scripts/tunnel/create_router_tunnel.sh
+++ scripts/tunnel/create_router_tunnel.sh
@@ -1,1 +1,1 @@
-bandd tx tunnel create-tunnel router router17c2txg2px6vna8a6v4ql4eh4ruvprerhytxvwt2ugp4qr473pajsyj9pgm 17000 0xDFCfEbF22e85193eDc37b8b136d4F3394987d1AE 300000 10000000 1000000000uband 1000 ./scripts/tunnel/signal_deviations.json --from requester --keyring-backend test --gas-prices 0.0025uband -y --chain-id bandchain
+bandd tx tunnel create-tunnel router 17000 0xDFCfEbF22e85193eDc37b8b136d4F3394987d1AE 300000 10000000 1000000000uband 1000 ./scripts/tunnel/signal_deviations.json --from requester --keyring-backend test --gas-prices 0.0025uband -y --chain-id bandchain
