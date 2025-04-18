121ff8697cf53664f081846db68e1ffc1f399faf
satawat thitisupakul <s.nack1919@gmail.com>
2025-04-18 17:30:24 +0700

    update contract (#648)
    Co-authored-by: satawatnack <satawat@bandprotocol.com>

--- scripts/tunnel/update_router_route.sh
+++ scripts/tunnel/update_router_route.sh
@@ -1,1 +1,1 @@
-bandd tx tunnel update-route router 1 17000 0xf6CE532a985D9F92Ee831196ADD8401669a2e019 10000001 --from requester --keyring-backend test --gas-prices 0.0025uband -y --chain-id bandchain
+bandd tx tunnel update-route router 1 17000 0xaCA1AfD10BB503A4Ea5A5e073ce261EE1dbc4F5A 1000000 --from requester --keyring-backend test --gas-prices 0.0025uband -y --chain-id bandchain
