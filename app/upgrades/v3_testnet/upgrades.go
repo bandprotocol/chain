e8efc11bdbb70e8dba3c4da9e2668e037d3ad3fd
Nattapat Iammelap <nattapat@Nattapats-MacBook-Pro.local>
2025-04-21 16:22:31 +0700

    add upgrade handler v3

---
+++ app/upgrades/v3_testnet/upgrades.go
@@ -0,0 +1,38 @@
+package v3
+
+import (
+	"context"
+
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+
+	"github.com/bandprotocol/chain/v3/app/keepers"
+)
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	keepers *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(c context.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		// Set param key table for params module migration
+		ctx := sdk.UnwrapSDKContext(c)
+
+		oracleParams := keepers.OracleKeeper.GetParams(ctx)
+		oracleParams.MaxCalldataSize = 512
+		oracleParams.MaxReportDataSize = 512
+		err := keepers.OracleKeeper.SetParams(ctx, oracleParams)
+		if err != nil {
+			return nil, err
+		}
+
+		vm, err := mm.RunMigrations(ctx, configurator, fromVM)
+		if err != nil {
+			return nil, err
+		}
+
+		return vm, nil
+	}
+}
