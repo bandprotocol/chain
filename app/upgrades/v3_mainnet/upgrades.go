1ee0894a491fdadb77627f4133bea106756eace8
Nattapat Iammelap <nattapat@Nattapats-MacBook-Pro.local>
2025-04-22 11:36:03 +0700

    add mint params check before upgrade

--- app/upgrades/v3_mainnet/upgrades.go
+++ app/upgrades/v3_mainnet/upgrades.go
@@ -15,16 +15,8 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
-	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
-	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
-	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
-	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 
 	"github.com/bandprotocol/chain/v3/app/keepers"
 	globalfeetypes "github.com/bandprotocol/chain/v3/x/globalfee/types"
@@ -42,23 +34,6 @@ func CreateUpgradeHandler(
 		for _, subspace := range keepers.ParamsKeeper.GetSubspaces() {
 			var keyTable paramstypes.KeyTable
 			switch subspace.Name() {
-			// cosmos-sdk types
-			case authtypes.ModuleName:
-				keyTable = authtypes.ParamKeyTable() //nolint:staticcheck
-			case banktypes.ModuleName:
-				keyTable = banktypes.ParamKeyTable() //nolint:staticcheck
-			case stakingtypes.ModuleName:
-				keyTable = stakingtypes.ParamKeyTable() //nolint:staticcheck
-			case minttypes.ModuleName:
-				keyTable = minttypes.ParamKeyTable() //nolint:staticcheck
-			case distrtypes.ModuleName:
-				keyTable = distrtypes.ParamKeyTable() //nolint:staticcheck
-			case slashingtypes.ModuleName:
-				keyTable = slashingtypes.ParamKeyTable() //nolint:staticcheck
-			case govtypes.ModuleName:
-				keyTable = govv1.ParamKeyTable() //nolint:staticcheck
-			case crisistypes.ModuleName:
-				keyTable = crisistypes.ParamKeyTable() //nolint:staticcheck
 			// ibc types
 			case ibctransfertypes.ModuleName:
 				keyTable = ibctransfertypes.ParamKeyTable()
@@ -90,10 +65,16 @@ func CreateUpgradeHandler(
 			return nil, err
 		}
 
-		mintParams, err := keepers.MintKeeper.Params.Get(ctx)
-		if err != nil {
-			return nil, err
+		var mintParams minttypes.Params
+		if has, err := keepers.MintKeeper.Params.Has(ctx); has && err != nil {
+			mintParams, err = keepers.MintKeeper.Params.Get(ctx)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			mintParams = minttypes.DefaultParams()
 		}
+
 		mintParams.BlocksPerYear = 31557600
 		err = keepers.MintKeeper.Params.Set(ctx, mintParams)
 		if err != nil {
