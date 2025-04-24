4aed982f0606eb7522359ae0fac585f62f39b5ba
taobun <bun@bandprotocol.com>
2025-04-24 14:43:15 +0700

    remove cdc

--- app/mempool/types.go
+++ app/mempool/types.go
@@ -4,7 +4,6 @@ import (
 	"reflect"
 	"slices"
 
-	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/authz"
 )
@@ -21,16 +20,16 @@ type TxWithInfo struct {
 
 type TxMatchFn func(sdk.Context, sdk.Tx) bool
 
-func NewTxMatchFn(cdc codec.Codec, msgs []sdk.Msg, onlyFree bool) TxMatchFn {
+func NewTxMatchFn(msgs []sdk.Msg, onlyFree bool) TxMatchFn {
 	msgTypes := make([]reflect.Type, len(msgs))
 
 	for i, msg := range msgs {
 		msgTypes[i] = reflect.TypeOf(msg)
 	}
 
-	var matchMsgFn func(sdk.Context, sdk.Msg, codec.Codec) bool
+	var matchMsgFn func(sdk.Context, sdk.Msg) bool
 
-	matchMsgFn = func(ctx sdk.Context, msg sdk.Msg, cdc codec.Codec) bool {
+	matchMsgFn = func(ctx sdk.Context, msg sdk.Msg) bool {
 		msgExec, ok := msg.(*authz.MsgExec)
 		if ok {
 			subMsgs, err := msgExec.GetMessages()
@@ -38,12 +37,11 @@ func NewTxMatchFn(cdc codec.Codec, msgs []sdk.Msg, onlyFree bool) TxMatchFn {
 				return false
 			}
 			for _, m := range subMsgs {
-				if !matchMsgFn(ctx, m, cdc) {
+				if !matchMsgFn(ctx, m) {
 					return false
 				}
 			}
 			return true
-
 		} else {
 			return slices.Contains(msgTypes, reflect.TypeOf(msg))
 		}
@@ -66,7 +64,7 @@ func NewTxMatchFn(cdc codec.Codec, msgs []sdk.Msg, onlyFree bool) TxMatchFn {
 			return false
 		}
 		for _, msg := range msgs {
-			if !matchMsgFn(ctx, msg, cdc) {
+			if !matchMsgFn(ctx, msg) {
 				return false
 			}
 		}
